b1aae9fe2073df559bacbbc33c40721ee62d1ee7
hwiyoon20010309 <boksil0101@gmail.com>
2025-05-01 22:44:23 +0900

    캡션별로 척도 랜덤으로 나오도록 수정

--- frontend/src/pages/Survey/SurveyDetail.js
+++ frontend/src/pages/Survey/SurveyDetail.js
@@ -6,14 +6,11 @@ import surveys from "../../data/SurveyData";
 
 const Wrapper = styled.div`
   font-family: Arial, sans-serif;
-  height: auto; /**100vh 해놓으면 스크롤 생김**/
+  height: auto;
   overflow-y: scroll;
   padding-top: 60px;
-
-  /* ✅ 스크롤바 숨기기 (모든 브라우저 대응) */
-  scrollbar-width: none; /* Firefox */
-  -ms-overflow-style: none; /* IE, Edge */
-
+  scrollbar-width: none;
+  -ms-overflow-style: none;
   &::-webkit-scrollbar {
     width: 0px;
     background: transparent;
@@ -61,15 +58,13 @@ const StartButton = styled.button`
   border: none;
   border-radius: 8px;
   cursor: pointer;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 
   &:hover {
     background-color: #3a6fbd;
   }
-
-  /* ✅ 가운데 정렬 */
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
 `;
 
 const SurveyDetail = () => {
@@ -78,41 +73,38 @@ const SurveyDetail = () => {
   const location = useLocation();
 
   const { image, path } = location.state || {};
-
   const matchedSurvey = surveys.find((item) => item.title === title);
   const captionList = matchedSurvey?.caption || [];
 
   return (
     <Wrapper>
       <CommonHeader />
-
       <Container>
         {path && <CategoryPath>{path}</CategoryPath>}
-
         <Title>
           LLM 기반 캡션과 사람의 생각이 일치하는 정도에 대한 설문조사입니다.
         </Title>
         <Instruction>이미지를 보시고 적절한 척도를 선택해주세요.</Instruction>
 
         <OptionBox>
-          <strong>1. 문화적으로 풍부한 캡션</strong>
+          <strong>문화적으로 풍부한 캡션 (5점)</strong>
           <br />- 문화적 세부 사항이 정확하게 담겨있으며, 역사적, 의미적, 상징적
           맥락이 풍부하게 표현된 캡션
         </OptionBox>
         <OptionBox>
-          <strong>2. 문화적으로 매우 적절한 캡션</strong>
+          <strong>문화적으로 매우 적절한 캡션 (4점)</strong>
           <br />- 적절한 이름, 국가 및 문화적 세부사항이 담겨있는 캡션
         </OptionBox>
         <OptionBox>
-          <strong>3. 문화적으로 올바른 캡션</strong>
+          <strong>문화적으로 올바른 캡션 (3점)</strong>
           <br />- 올바른 이름, 국가만이 담겨있는 캡션
         </OptionBox>
         <OptionBox>
-          <strong>4. 중립적 또는 일반적인 캡션</strong>
+          <strong>중립적 또는 일반적인 캡션 (2점)</strong>
           <br />- 문화적 요소는 포함되지 않고, 일반적인 설명이 포함된 캡션
         </OptionBox>
         <OptionBox>
-          <strong>5. 문화적으로 부적절한 캡션</strong>
+          <strong>문화적으로 부적절한 캡션 (1점)</strong>
           <br />- 잘못된 이름이나 국가를 표현하거나 올바르지 않은 문화적 맥락을
           담고있는 캡션
         </OptionBox>
